3b4fbd99c2ef306e06b22211005e76fcdc87b5e0
sgengler <sgengler@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-05 14:16:21 +0000

    Tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/acf-pro-sync/tags/1.0.1@1060430 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/sync.php
+++ views/sync.php
@@ -8,11 +8,11 @@ $filename = get_template_directory()  . '/acf-json';
 	<div id="welcome-panel" class="welcome-panel acf-sync-panel" style="padding: 20px;">
 		<h3><i class="dashicons dashicons-download"></i> ACF Pro JSON Sync</h3>
 
-		<?php if (!check_json_dirs()): ?>
+		<?php if (!acf_sync_check_json_dirs()): ?>
 			<p>Syncing will remove all ACF fields from the database and replace them with json fields from your acf-json folder.</p>
 			<h4>The following json files will be pulled into the database:</h4>
 			<ul class="sync-file-list">
-				<?php $files = get_acf_fields_list(); ?>
+				<?php $files = acf_sync_get_fields_list(); ?>
 				<?php foreach($files as $file): ?>
 					<li><a target="_blank" href="<?php echo get_bloginfo('url') .  $file['path']; ?>"><i class="dashicons dashicons-media-default"></i><?php echo $file['title']; ?></a></li>
 				<?php endforeach; ?>
@@ -22,7 +22,7 @@ $filename = get_template_directory()  . '/acf-json';
 			<div class="acf-sync-warning">
 				<h4>the following json directories are missing from your theme:</h4>
 				<ul>
-					<?php foreach (check_json_dirs() as $dir) { ?>
+					<?php foreach (acf_sync_check_json_dirs() as $dir) { ?>
 						<li class="welcome-icon dashicons-category"><?php echo $dir ?></li>
 					<?php } ?>
 				</ul>
